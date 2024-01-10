e02fc56af4884f24b8ccd4ff38ca0b6f53f277be
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2024-01-10 19:29:21 -0300

    Adicionar arquivos de projeto.

---
+++ TesteAdverhouseApi/Model/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace TesteAdverhouseApi.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string NomeCliente { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        public string CodigoCnae { get; set; }
+        public string Endereco { get; set; }
+    }
+}
