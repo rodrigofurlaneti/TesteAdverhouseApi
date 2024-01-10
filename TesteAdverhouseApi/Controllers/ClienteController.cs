ee9829052dd345cd34354ae0ed05913143e3ae99
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2024-01-10 19:58:51 -0300

    Add test

---
+++ TesteAdverhouseApi/Controllers/ClienteController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteAdverhouseApi.Infrastructure;
+using TesteAdverhouseApi.Model;
+
+namespace TesteAdverhouseApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : Controller
+    {
+        [HttpGet(Name = "GetClienteAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ClienteModel>))]
+        public async Task<IActionResult> GetBuyersAsync()
+        {
+            try
+            {
+                ClienteRepository dal = new ClienteRepository();
+                var ret = await dal.GetAllAsync();
+                return Ok(ret);
+            }
+            catch (Exception ex)
+            {
+                string mensagem = "Erro ao consumir a controller Cliente, rota GetCliente " + ex.Message;
+                throw new ArgumentNullException(mensagem);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ClienteModel>))]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                ClienteRepository dal = new ClienteRepository();
+                var ret = await dal.GetByIdAsync(id);
+                return Ok(ret);
+            }
+            catch (Exception ex)
+            {
+                string mensagem = "Erro ao consumir a controller Cliente, rota GetByIdAsync " + ex.Message;
+                throw new ArgumentNullException(mensagem);
+            }
+        }
+
+        [HttpPost(Name = "InsertCliente")]
+        public async Task Post(ClienteModel buyerModel)
+        {
+            try
+            {
+                ClienteRepository dal = new ClienteRepository();
+                await dal.InsertAsync(buyerModel);
+            }
+            catch (Exception ex)
+            {
+                string mensagem = "Erro ao consumir a controller Cliente, rota Post " + ex.Message;
+                throw new ArgumentNullException(mensagem);
+            }
+
+        }
+
+        [HttpPut(Name = "UpdateCliente")]
+        public async Task Update(ClienteModel buyerModel)
+        {
+            try
+            {
+                ClienteRepository dal = new ClienteRepository();
+                await dal.UpdateAsync(buyerModel);
+            }
+            catch (Exception ex)
+            {
+                string mensagem = "Erro ao consumir a controller Cliente, rota Put " + ex.Message;
+                throw new ArgumentNullException(mensagem);
+            }
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task Delete(int id)
+        {
+            try
+            {
+                ClienteRepository dal = new ClienteRepository();
+                await dal.DeleteAsync(id);
+            }
+            catch (Exception ex)
+            {
+                string mensagem = "Erro ao consumir a controler Buyer, rota DeleteAsync " + ex.Message;
+                throw new ArgumentNullException(mensagem);
+            }
+
+        }
+    }
+}
